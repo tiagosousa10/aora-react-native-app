203bbd3221b0494f49470c9beedcf41b1bba2000
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-20 17:48:08 +0000

    add assets and constants, fix tailwindcss and babel version bug

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
 
-export default function App() {
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>HELLO!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Stack>
+      <Stack.Screen  name='index' options={{ headerShown: false }} />
+    </Stack>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default RootLayout
+
