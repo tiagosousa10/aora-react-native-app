bab7e8658c85142117f3c8cb62a7fce89a7eef83
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-20 18:17:17 +0000

    create all the tabs, and start with the first layout

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,55 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Tabs, Redirect} from 'expo-router'
+
+import {icons} from '../../constants'
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-[10px]`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+      <>
+        <Tabs
+          screenOptions={{
+            tabBarShowLabel:false
+          }}
+        >
+          <Tabs.Screen 
+            name='home'
+            options={{
+              title: 'Home',
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => (
+                <TabIcon 
+                  icon={icons.home}
+                  color={color}
+                  name="Home"
+                  focused={focused}
+                />
+              )
+            }}
+          />
+        </Tabs>
+      </>
+  )
+}
+
+export default TabsLayout
+
