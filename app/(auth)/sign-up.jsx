b073ea4d9bc1215131040fc84874b1fd9e6a7adb
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-21 18:30:54 +0000

    create user and signIn with appwrite

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -5,7 +5,7 @@ import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link } from 'expo-router'
-
+import {createUser} from '../../lib/appwrite.js'
 
 const SignUp = () => {
   const [form,setForm] = useState({
@@ -16,7 +16,7 @@ const SignUp = () => {
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const submit = () => {
-
+    createUser()
   }
 
   return (
